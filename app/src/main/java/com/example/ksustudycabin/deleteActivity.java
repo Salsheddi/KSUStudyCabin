30eee90952c8185b11bbea8d8b59419bd1a0a51f
Tarfah-Amin <tarfah.ateeq@hotmail.com>
2024-03-28 23:11:25 +0300

    Done!!!

--- app/src/main/java/com/example/ksustudycabin/deleteActivity.java
+++ app/src/main/java/com/example/ksustudycabin/deleteActivity.java
@@ -11,14 +11,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class UserReservationActivity extends AppCompatActivity {
+public class deleteActivity extends AppCompatActivity {
     ImageButton delete , edit;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_user_reservation);
+        setContentView(R.layout.activity_delete);
 
         delete = findViewById(R.id.deletebtn);
         edit = findViewById(R.id.editbtn);
@@ -40,7 +39,6 @@ public class UserReservationActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
